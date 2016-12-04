7e72dd0d9889ea3b0b10fef8fa93ebbbee57cb27
Artur de Araujo <artur.at4@gmail.com>
2016-12-04 00:29:37 -0300

    commit 2
    modo estável com 4 clientes sem decrementa-los quando encerrado

--- mainwindow_login.cpp
+++ mainwindow_login.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow_login.h"
 #include "ui_mainwindow_login.h"
-#include"client.h"
+#include "client.h"
+#include "mainwindow_mainhall.h"
+#include "ui_mainwindow_mainhall.h"
+
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include <QMessageBox>
 
-MainWindow_login::MainWindow_login(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow_login)
+using namespace std;
+int i=0;
+int j=4;
+
+MainWindow_login::MainWindow_login(QWidget *parent) :    QMainWindow(parent),    ui(new Ui::MainWindow_login)
 {
     ui->setupUi(this);
 }
@@ -21,5 +26,43 @@ void MainWindow_login::on_pushButton_login_clicked()
 {
     QString nameTyped = ui->lineEdit_username->text();
     QString passTyped = ui->lineEdit_password->text();
+    if(nameTyped=="123"&&passTyped=="123")
+    {
+        if(j>0)
+        {
+            j--;
+            switch (i) {
+            case 0:
+                QMessageBox::information(this,"Login aprovado","Bem-vindo, cliente_01 :D");
+                mainHall01.show();
+                i++;
+                break;
+            case 1:
+                QMessageBox::information(this,"Login aprovado","Bem-vindo, cliente_02 :D");
+                mainHall02.show();
+                i++;
+                break;
+            case 2:
+                QMessageBox::information(this,"Login aprovado","Bem-vindo, cliente_03 :D");
+                mainHall03.show();
+                i++;
+                break;
+            case 3:
+                QMessageBox::information(this,"Login aprovado","Bem-vindo, cliente_04 :D");
+                mainHall04.show();
+                i++;
+                break;
+            }
+
+        }
+        else
+        {
+            QMessageBox::warning(this,"Desculpe-nos D:", "Nossa política de qualidade estipula limite de máximo para melhor atendê-lo. Tente novamente em instantes. Enquanto isso, tire uma folga no nosso bar.");
+        }
+    }
+    else
+    {
+        QMessageBox::warning(this,"Algo estranho","verifique seu usúario ou senha e tente novamente");
+    }
 
 }
--- mainwindow_login.h
+++ mainwindow_login.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_LOGIN_H
 #define MAINWINDOW_LOGIN_H
-
 #include <QMainWindow>
+#include<vector>
+#include"mainwindow_mainhall.h"
+#include "restaurant.h"
 
 namespace Ui {
 class MainWindow_login;
@@ -20,6 +22,23 @@ private slots:
 
 private:
     Ui::MainWindow_login *ui;
+    vector<Ui::MainWindow_MainHall> visionClient[16];//não consegui implementar essa instrução :/
+    MainWindow_MainHall mainHall01;//resolvi deixar na brutalidade :D D:
+    MainWindow_MainHall mainHall02;
+    MainWindow_MainHall mainHall03;
+    MainWindow_MainHall mainHall04;
+    MainWindow_MainHall mainHall05;
+    MainWindow_MainHall mainHall06;
+    MainWindow_MainHall mainHall07;
+    MainWindow_MainHall mainHall08;
+    MainWindow_MainHall mainHall09;
+    MainWindow_MainHall mainHall10;
+    MainWindow_MainHall mainHall11;
+    MainWindow_MainHall mainHall12;
+    MainWindow_MainHall mainHall13;
+    MainWindow_MainHall mainHall14;
+    MainWindow_MainHall mainHall15;
+    MainWindow_MainHall mainHall16;
 };
 
 #endif // MAINWINDOW_LOGIN_H
